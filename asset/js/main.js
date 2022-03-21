a0e72c9c4a028e141adf3aa5d351e110a65333c9
andrea1632 <andrea.bellucci10@libero.it>
2022-03-21 16:43:06 +0100

    es completo

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,22 +1,47 @@
+//variabili globali
 let grid = document.getElementById("grid");
 let btn = document.getElementById("btn");
 let select = document.getElementById("select");
 
 btn.addEventListener("click",
 function(){
+    //condizioni per determinare la difficoltà scelta dall'utente
     if(select.value == "facile"){
         for(let i = 1; i <= 49; i++){
-            grid.innerHTML += `<div style="width: calc(100% / 7); height: calc(100% / 7);" class="box">${i}</div>`
+            let content = document.createElement("div");
+            content.classList.add("boxEasy");
+            content.innerHTML = i;
+            grid.appendChild(content);
+            //funzione per colorare il box al click
+            content.addEventListener("click",
+                function colorOnClick(){
+                this.classList.add("bgLightBlue");
+            })
         }
     } else if(select.value == "medio"){
         for(let i = 1; i <= 81; i++){
-            grid.innerHTML += `<div style="width: calc(100% / 9); height: calc(100% / 9);" class="box">${i}</div>`
+            let content = document.createElement("div");
+            content.classList.add("boxMedium");
+            content.innerHTML = i;
+            grid.appendChild(content);
+            //funzione per colorare il box al click
+            content.addEventListener("click",
+                function colorOnClick(){
+                this.classList.add("bgLightBlue");
+            })
         }
     } else if(select.value == "difficile"){
         for(let i = 1; i <= 100; i++){
-            grid.innerHTML += `<div style="width: calc(100% / 10); height: calc(100% / 10);" class="box">${i}</div>`
+            let content = document.createElement("div");
+            content.classList.add("boxHard");
+            content.innerHTML = i;
+            grid.appendChild(content);
+            //funzione per colorare il box al click
+            content.addEventListener("click",
+                function colorOnClick(){
+                this.classList.add("bgLightBlue");
+            })
         }
     }
-    
 }
 )
