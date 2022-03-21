43aeaf1eea76a40554c4e3349c326a9735e778cd
andrea1632 <andrea.bellucci10@libero.it>
2022-03-21 16:07:51 +0100

    box generator

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,16 +6,17 @@ btn.addEventListener("click",
 function(){
     if(select.value == "facile"){
         for(let i = 1; i <= 49; i++){
-            grid.innerHTML += `<div class="box">${i}</div>`
+            grid.innerHTML += `<div style="width: calc(100% / 7); height: calc(100% / 7);" class="box">${i}</div>`
         }
     } else if(select.value == "medio"){
         for(let i = 1; i <= 81; i++){
-            grid.innerHTML += `<div class="box">${i}</div>`
+            grid.innerHTML += `<div style="width: calc(100% / 9); height: calc(100% / 9);" class="box">${i}</div>`
         }
     } else if(select.value == "difficile"){
         for(let i = 1; i <= 100; i++){
-            grid.innerHTML += `<div class="box">${i}</div>`
+            grid.innerHTML += `<div style="width: calc(100% / 10); height: calc(100% / 10);" class="box">${i}</div>`
         }
     }
+    
 }
 )
